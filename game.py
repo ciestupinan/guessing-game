288d42cb07290fbf23546de06810251935032e7b
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-03 16:04:50 -0700

    Greeting the player

---
+++ game.py
@@ -0,0 +1,22 @@
+"""A number-guessing game."""
+
+# Put your code here
+import random
+def game():
+	name = input("Howdy, what's your name?")
+	"""secret_number = random.randint(0,101)
+	print(name + " I'm thinking of a number between 1 and 100. Try to guess my number.")
+	count = 0
+	while True:
+		guess = int(input("Your guess?"))
+		count +=1
+		if guess > secret_number:
+			print("Your guess is too high, try again.")
+		elif guess < secret_number:
+			print("Your guess is too low, try again.")
+		else:
+			print("Well done, " + name + "! You found my number in", count, "tries!")
+			break
+"""
+
+game()
